294d28045f63253a7feeddb77fcdbfe9fccc648f
FYLSunghwan <sunghwan519@hotmail.com>
2019-03-27 16:48:39 +0900

    w4

--- w4/190327_w4_6.py
+++ w4/190327_w4_6.py
@@ -1,8 +1,14 @@
 import random
 
 regame=1
+cntwin=0
+cnttot=0
+
+def cal():
+	return cntwin/cnttot*100
 
 while regame:
+	cnttot+=1
 	rnd=random.randint(0,2)
 	inp=input()
 	user=0
@@ -12,7 +18,10 @@ while regame:
 	else: user=-1
 	if user==rnd: print("bigim")
 	if (user+1)%3==rnd: print("You Lose!")
-	else: print("You Win!")
+	else: 
+		print("You Win!")
+		cntwin+=1
 	print("Regame?:")
 	inp=input()
 	if inp=="no": regame=0
+print(cal(),"%")
