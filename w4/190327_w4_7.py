35ae12c3bbb5431a26199f07807ece659b4ffbaf
FYLSunghwan <sunghwan519@hotmail.com>
2019-03-27 17:13:20 +0900

    w4

--- w4/190327_w4_7.py
+++ w4/190327_w4_7.py
@@ -1,5 +1,5 @@
 n=5
-
+cn=0
 def drawline(m):
 	if m%2:
 		print("*"*m)
@@ -9,13 +9,17 @@ def drawline(m):
 	print("*"*m+" "*(2*(n-m)-1)+"*"*m)
 
 n=int(input())
+cn+=(n*2-1)*2
 drawline(n*2-1)
 i=n-1
 while i:
 	drawline(i*2)
+	cn+=i*2
 	i-=1
-i=1
+i=2
 while i<n:
 	drawline(i*2)
+	cn+=i*2
 	i+=1
 drawline(n*2-1)
+print(cn)
