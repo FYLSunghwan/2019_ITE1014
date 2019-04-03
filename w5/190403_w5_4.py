481c3c12470639eedb6583c6c937c58f6b0110a7
FYLSunghwan <sunghwan519@hotmail.com>
2019-04-03 16:52:58 +0900

    w5

--- w5/190403_w5_4.py
+++ w5/190403_w5_4.py
@@ -9,16 +9,22 @@ def sort():
 
 import random
 max=-1
+maxidx=0
 min=1001
+minidx=0
 for i in range(0,50):
 	ls.append(random.randint(0,1000))
-	if ls[i]>=max: max=ls[i]
-	if ls[i]<=min: min=ls[i]
+	if ls[i]>=max: 
+		max=ls[i]
+		maxidx=i
+	if ls[i]<=min:
+		min=ls[i]
+		minidx=i
 	print(ls[i],end=' ')
 print()
 sort()
 for i in range(0,50):
 	print(ls[i], end=' ')
 print()
-print("max value:",max)
-print("min value:",min)
+print("max value:",max,"idx:",maxidx)
+print("min value:",min,"idx:",minidx)
