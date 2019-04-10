1eadb31b92c2d724b9007fdbdae65698618b389e
FYLSunghwan <sunghwan519@hotmail.com>
2019-04-10 22:19:43 +0900

    190410_w6_7.py Fixed

--- w6/190410_w6_7.py
+++ w6/190410_w6_7.py
@@ -3,19 +3,24 @@ import math
 cnt=0
 sm=0
 var=0
-img = imageio.imread("HDR.jpg")
-imgsum = sum(img)
 
+test = [1,2,3,4]
+
+im = imageio.imread("HDR.jpg")
+img = im.tolist()
+imgsum = sum(img, [])
 ls = [0 for i in range(256)]
 for i in imgsum:
 	ls[i]+=1
 
-ls[0]=ls[0]/len(ls)
+print(len(imgsum))
+
+ls[0]=ls[0]/len(imgsum)
 for i in range(1,256):
-	ls[i]=ls[i]/len(imgsum)+ls[i-1]
+	ls[i]=ls[i-1]+ls[i]/len(imgsum)
 
 for i in range(len(img)):
 	for j in range(len(img[i])):
-		img[i][j]=math.floor((255)*ls[img[i][j]])
+		im[i][j]=math.floor((256)*ls[im[i][j]])
 
-imageio.imwrite('HDRCOM.jpg',img)
+imageio.imwrite('HDRCOM.jpg',im)
