8e9023f80db1565c892da66c583f4ea28f67c503
FYLSunghwan <sunghwan519@hotmail.com>
2019-04-17 16:36:54 +0900

    w7

--- w7/190417_w7_2.py
+++ w7/190417_w7_2.py
@@ -1,10 +1,13 @@
-n = int(input())
-for i in range(1,n+1):
-	cnt = i
+def count(cnt):
 	ans=0
 	while cnt:
 		if cnt%10 == 3 or  (cnt%10)==6 or cnt%10 == 9: ans+=1
 		cnt//=10
+	return ans
+
+n = int(input())
+for i in range(1,n+1):
+	ans=count(i)
 	if ans:
 		for i in range(ans): 
 			print("clap ",end='')
