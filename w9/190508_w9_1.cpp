ee8e1c5e0879f0c0cdc17e8b6f2928e6f75c18d7
FYLSunghwan <sunghwan519@hotmail.com>
2019-05-08 16:20:15 +0900

    w9

--- w9/190508_w9_1.cpp
+++ w9/190508_w9_1.cpp
@@ -1,25 +1,23 @@
+#include <time.h>
 #include <cstdio>
+#include <cstdlib>
+
 int main() {
-    char name[256], gender[256], dest[256];
-    int birth, trip;
+     char str[256];
+     int k,r,cnt=0;
+     srand(time(NULL));
+     r=rand()%20+1;
+     printf("Hello! What is your name?\n");
+     scanf("%s",str);
+     while(1) {
+         ++cnt;
+         printf("Take a guess.\n" );
+         scanf("%d",&k);
+         if(k<r) printf("Your guess is too low.\n");
+         else if(k>r) printf("Your guess is to high.\n");
+         else {
+             printf("Good Job,%s! You guessed my number in %d guesses!\n",str,cnt);
 
-    printf("Input your name:");
-    scanf("%s",name);
-    printf("Gender:");
-    scanf("%s",gender);
-    printf("Day of birth:");
-    scanf("%d",&birth);
-    printf("Destination:");
-    scanf("%s",dest);
-    printf("Period of trip:");
-    scanf("%d",&trip);
-
-    printf("===================================\n");
-    printf("NAME:             %s\n",name);
-    printf("GENDER:           %s\n",gender);
-    printf("DAY OF BIRTH:     %d\n",birth);
-    printf("DESTINATION:      %s\n",dest);
-    printf("PERIOD OF TRIP:   %d\n",trip);
-    printf("===================================\n");
-    return 0;
+         }
+     }
 }
